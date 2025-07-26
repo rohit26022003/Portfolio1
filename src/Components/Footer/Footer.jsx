1f9c36d5fd264e7aff9368d2315078cc8ad436fa
rohit26022003 <shrivastavarohit701@gmail.com>
2025-07-26 20:02:36 +0530

    Fix asset path issue

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import './Footer.css';
-import userIcon from '../../Assets/user_icon.svg';
+import React from 'react'
+import './Footer.css'
+// import footer_logo from '../../Assets/footer_logo.svg'
+import user_icon from '../../Assets/user_icon.svg'
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-top">
         <div className="footer-top-left">
-          <p>
-            I am a frontend developer. I can provide clean code and pixel-perfect design. I also make websites more interactive with web animations.
-          </p>
+          {/* <img src={footer_logo} alt="" /> */}
+          <p>I am a fontend developer, I can provide clean code and pixel-perfect design. I also make websites more interactive with web animations.</p>
+
         </div>
         <div className="footer-top-right">
           <div className="footer-email-input">
-            <img src={userIcon} alt="user icon" />
+            <img src={user_icon} alt="" />
             <input type="email" placeholder="Enter your email" />
           </div>
           <div className="footer-subscribe">Subscribe</div>
@@ -21,15 +22,16 @@ const Footer = () => {
       </div>
       <hr />
       <div className="footer-bottom">
-        <p className="footer-bottom-left">© 2025 Rohit Shrivastava All rights reserved.</p>
-        <div className="footer-bottom-right">
-          <p>Term of Services</p>
-          <p>Privacy Policy</p>
-          <p>Connect with me</p>
-        </div>
+        <p className="footer-bottom-left">
+          © 2025 Rohit Shrivastava All rights reserved.</p>
+          <div className="footer-bottom-right">
+            <p>Term of Services</p>
+            <p>Privacy Policy</p>
+            <p>Connect with me</p>
+          </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
